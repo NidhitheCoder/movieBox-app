21ed7b02abfe601fda1866a506217b4f21f6528d
NidhitheCoder <nidhpv@gmail.com>
2020-11-19 11:46:24 +0530

    Initial commit

---
+++ movie-box/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import logo from './assets/iconMovie.png';
+
+function App() {
+  return (
+    <div className="App">
+      <img src={logo} alt ="Icon"/>
+    </div>
+  );
+}
+
+export default App;
