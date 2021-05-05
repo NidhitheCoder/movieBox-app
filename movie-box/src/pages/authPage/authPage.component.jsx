14d26ea5e0c2bfbb5937a4a8993132c65557849a
NidhitheCoder <nidhpv@gmail.com>
2021-05-05 23:18:32 +0530

    create page and route login

---
+++ movie-box/src/pages/authPage/authPage.component.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const authPage = () => {
+  return <div style={{display:"flex",justifyContent:'center', minHeight:'100vh',color:'orange',width:'100vw'}}>Login page</div>;
+};
+
+export default authPage;
