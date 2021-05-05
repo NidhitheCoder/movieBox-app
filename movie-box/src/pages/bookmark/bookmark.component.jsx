12481fc6a89d82acd279791e1e14a89fb480980c
NidhitheCoder <nidhpv@gmail.com>
2021-05-05 22:48:11 +0530

    implement search through movie list

--- movie-box/src/pages/bookmark/bookmark.component.jsx
+++ movie-box/src/pages/bookmark/bookmark.component.jsx
@@ -3,17 +3,28 @@ import "./bookmark.styles.modules.scss";
 import { connect } from "react-redux";
 import Container from "../../components/container/container.component";
 
-const Bookmark = props => {
-  const { bookmarkList, history } = props;
+const Bookmark = (props) => {
+  const { bookmarkList, history, searchKeyWord } = props;
+
+  let moviesList;
+  if (searchKeyWord !== "") {
+    moviesList = bookmarkList.filter(({ Title }) =>
+      Title.toLowerCase().includes(searchKeyWord.toLowerCase())
+    );
+  } else {
+    moviesList = bookmarkList;
+  }
+
   return (
     <div className="bookmark-container">
-      <Container movieList={bookmarkList} history={history} />
+      <Container movieList={moviesList} history={history} />
     </div>
   );
 };
 
-const mapStateToProps = state => ({
-  bookmarkList: state.bookmark.bookmark
+const mapStateToProps = (state) => ({
+  bookmarkList: state.bookmark.bookmark,
+  searchKeyWord: state.searchKey.searchKeyWord,
 });
 
 export default connect(mapStateToProps)(Bookmark);
